11f26aae0d74e7d7d9e7782627a6be48ea6cdf4f
andrew <p-a-k-12@mail.ru>
2022-09-19 23:41:12 +0300

    add ContentParser

--- src/main/kotlin/parsing/ContentParser.kt
+++ src/main/kotlin/parsing/ContentParser.kt
@@ -2,8 +2,75 @@ package parsing
 
 import Context
 import entities.CLIEntity
+import exceptions.ParseException
 
 class ContentParser {
+    private class MarksStateHolder {
+        private enum class State {  // quotation marks or not
+            STANDARD,
+            ONE_MARK,   // '
+            TWO_MARK    // "
+        }
 
-    fun parse(input: String, context: Context): List<CLIEntity> = TODO()
+        var state = State.STANDARD
+        fun one() = when(state) {
+            State.STANDARD -> { state = State.ONE_MARK }
+            State.ONE_MARK -> { state = State.STANDARD }
+            else -> {}
+        }
+
+        fun two() = when(state) {
+            State.STANDARD -> { state = State.TWO_MARK }
+            State.TWO_MARK -> { state = State.STANDARD }
+            else -> {}
+        }
+
+        val isStandard: Boolean
+            get() = state == State.STANDARD
+    }
+
+    private val cliEntityCreator = CLIEntityCreator()
+
+    private fun splitSpace(input: String): List<String> {
+        val builder = StringBuilder()
+        val splitters = mutableListOf<String>()
+        val marksStateHolder = MarksStateHolder()
+        input.forEach { ch -> when(ch) {
+            '\'' -> {
+                marksStateHolder.one()
+                builder.append(ch)
+            }
+            '"' -> {
+                marksStateHolder.two()
+                builder.append(ch)
+            }
+            ' ' -> {
+                if (builder.isNotEmpty()) {
+                    if (marksStateHolder.isStandard) {
+                        splitters.add(builder.toString())
+                        builder.clear()
+                    } else builder.append(ch)
+                }
+            }
+            else -> builder.append(ch)
+        }}
+        return splitters
+    }
+
+    fun parse(input: String, context: Context): List<CLIEntity> {
+        val splitters = splitSpace(input)
+//        println(splitters)
+        val cliEntities = mutableListOf<CLIEntity>()
+        val firstEntity = try {
+             cliEntityCreator.createKeyword(splitters.first(), context)
+        } catch (_: ParseException) {
+            cliEntityCreator.createInitialization(splitters.first())
+        }
+        cliEntities.add(firstEntity)
+        splitters.drop(1).forEach { arg ->
+            val entity = cliEntityCreator.createArgument(arg)
+            cliEntities.add(entity)
+        }
+        return cliEntities
+    }
 }
