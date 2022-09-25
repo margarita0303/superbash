4a41d4a5c8de0889ca627e1a98b660ae2810183e
Daniil Kulikov <kuldanvit@gmail.com>
2022-09-25 20:50:51 +0300

    + pwd

--- src/main/kotlin/entities/executors/PwdExecutor.kt
+++ src/main/kotlin/entities/executors/PwdExecutor.kt
@@ -2,8 +2,10 @@ package entities.executors
 
 import entities.Argument
 import entities.Keyword
+import java.nio.file.Path
 import java.util.*
+import kotlin.io.path.name
 
-class PwdExecutor: Keyword {
-    override fun execute(arguments: List<Argument>): Optional<String> = TODO()
+class PwdExecutor(private val curPath: Path): Keyword {
+    override fun execute(arguments: List<Argument>): Optional<String> = Optional.of(curPath.name)
 }
