26b234325221b5e6ff5a3dd53341df3da76ac087
Daniil Kulikov <kuldanvit@gmail.com>
2022-09-25 20:51:09 +0300

    + echo

--- src/main/kotlin/entities/executors/EchoExecutor.kt
+++ src/main/kotlin/entities/executors/EchoExecutor.kt
@@ -5,5 +5,12 @@ import entities.Keyword
 import java.util.*
 
 class EchoExecutor: Keyword {
-    override fun execute(arguments: List<Argument>): Optional<String> = TODO()
+    override fun execute(arguments: List<Argument>): Optional<String> {
+        var s = ""
+        for (argument in arguments) {
+            s += argument.getArgument()
+        }
+
+        return Optional.of(s)
+    }
 }
