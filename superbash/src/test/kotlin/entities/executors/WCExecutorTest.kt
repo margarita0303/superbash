be8567dc66d050213873ef23c858b8cf1479a1c9
mandelshtamd <mandelstudy@gmail.com>
2022-09-27 23:37:54 +0300

    rename tests

--- superbash/src/test/kotlin/entities/executors/WCExecutorTest.kt
+++ superbash/src/test/kotlin/entities/executors/WCExecutorTest.kt
@@ -15,14 +15,14 @@ class WCExecutorTest {
     private val wcExecutor = WCExecutor(curPath)
 
     @Test
-    fun testCatFile1() {
+    fun testWcFile1() {
         val file1 = listOf<Argument>(CLIArgument(FILE1_TEST.toString()))
         val result = wcExecutor.execute(file1)
         Assert.assertEquals("1 1 13 test_file1.txt\n", result.get())
     }
 
     @Test
-    fun testCatFile1AndFile2() {
+    fun testWcFile1AndFile2() {
         val file1AndFile2 = listOf<Argument>(CLIArgument(FILE1_TEST.toString()), CLIArgument(FILE2_TEST.toString()))
         val result = wcExecutor.execute(file1AndFile2)
         Assert.assertEquals("1 1 13 test_file1.txt\n1 1 8 test_file2.txt\n2 2 21 total\n", result.get())
