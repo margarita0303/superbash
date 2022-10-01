bd4d38abc6b87b1c5343a4b09bd333b9bd9cd710
Daniil Kulikov <kuldanvit@gmail.com>
2022-10-01 23:52:51 +0300

    executors tests fixes

--- superbash/src/test/kotlin/entities/executors/WCExecutorTest.kt
+++ superbash/src/test/kotlin/entities/executors/WCExecutorTest.kt
@@ -18,18 +18,34 @@ class WCExecutorTest {
     fun testWcFile1() {
         val file1 = listOf<Argument>(CLIArgument(FILE1_TEST.toString()))
         val result = wcExecutor.execute(file1)
-        Assert.assertEquals("1 1 13 test_file1.txt\n", result.get())
+        Assert.assertEquals("$FILE1_LINES $FILE1_WORDS $FILE1_BYTES test_file1.txt\n", result.get())
     }
 
     @Test
     fun testWcFile1AndFile2() {
         val file1AndFile2 = listOf<Argument>(CLIArgument(FILE1_TEST.toString()), CLIArgument(FILE2_TEST.toString()))
         val result = wcExecutor.execute(file1AndFile2)
-        Assert.assertEquals("1 1 13 test_file1.txt\n1 1 8 test_file2.txt\n2 2 21 total\n", result.get())
+        Assert.assertEquals(
+            "$FILE1_LINES $FILE1_WORDS $FILE1_BYTES test_file1.txt\n$FILE2_LINES $FILE2_WORDS $FILE2_BYTES test_file2.txt\n$TOTAL_LINES $TOTAL_WORDS $TOTAL_BYTES total\n",
+            result.get()
+        )
     }
 
     companion object {
         private val FILE1_TEST = Paths.get("src/test/resources/test_file1.txt").absolute().toFile()
+        private val FILE1_CONTENT = FILE1_TEST.readText()
+        private val FILE1_WORDS = FILE1_CONTENT.split(' ').size
+        private val FILE1_LINES = FILE1_CONTENT.count { it == '\n' }
+        private val FILE1_BYTES = FILE1_CONTENT.toByteArray().size
+
         private val FILE2_TEST = Paths.get("src/test/resources/test_file2.txt").absolute().toFile()
+        private val FILE2_CONTENT = FILE2_TEST.readText()
+        private val FILE2_WORDS = FILE2_CONTENT.split(' ').size
+        private val FILE2_LINES = FILE2_CONTENT.count { it == '\n' }
+        private val FILE2_BYTES = FILE2_CONTENT.toByteArray().size
+
+        private val TOTAL_WORDS = FILE1_WORDS + FILE2_WORDS
+        private val TOTAL_LINES = FILE1_LINES + FILE2_LINES
+        private val TOTAL_BYTES = FILE1_BYTES + FILE2_BYTES
     }
 }
