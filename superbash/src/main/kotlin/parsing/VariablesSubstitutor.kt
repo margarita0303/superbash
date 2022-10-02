315a4b98163b7fc724f975647d8f5c99e42023d9
mandelshtamd <mandelstudy@gmail.com>
2022-10-02 13:31:17 +0300

    add comments

--- superbash/src/main/kotlin/parsing/VariablesSubstitutor.kt
+++ superbash/src/main/kotlin/parsing/VariablesSubstitutor.kt
@@ -4,20 +4,29 @@ import Context
 import parsing.helpers.ImportantChars
 import parsing.helpers.splitBySpaceAndPipe
 
+/**
+ * A class that substitutes variables in content if content is in quote marks or without any marks
+ */
 class VariablesSubstitutor {
     private val bashVariableRegex = "\\$[a-zA-Z_][a-zA-Z_0-9]*".toRegex()
 
+    /**
+     * Make a substitution in `content` with variables in `context`
+     */
     fun substitute(content: String, context: Context): String {
         val splittedContent = splitBySpaceAndPipe(content)
         val substitutedContent = splittedContent.map {
             when {
-                it.isDoubleQuotedString() || it.isNotQuotedString() -> substituteOneString(it, context)
+                it.isQuoteMarkString() || it.isNotMarkedString() -> substituteOneString(it, context)
                 else -> it
             }
         }
         return substitutedContent.joinToString(" ")
     }
 
+    /**
+     * Make a substitution in one string
+     */
     private fun substituteOneString(content: String, context: Context): String {
         var substitutedContent = content
         bashVariableRegex.findAll(content).toMutableList().reversed().forEach { match ->
@@ -28,11 +37,17 @@ class VariablesSubstitutor {
         return substitutedContent
     }
 
-    private fun String.isDoubleQuotedString(): Boolean {
+    /**
+     * Check if string is inside quote marks
+     */
+    private fun String.isQuoteMarkString(): Boolean {
         return first() == ImportantChars.QUOTE_MARK_CHAR && last() == ImportantChars.QUOTE_MARK_CHAR
     }
 
-    private fun String.isNotQuotedString(): Boolean {
-        return !contains(ImportantChars.QUOTE_MARK_CHAR) && !contains(ImportantChars.SINGLE_MARK_CHAR)
+    /**
+     * Check if string has no border marks
+     */
+    private fun String.isNotMarkedString(): Boolean {
+        return !contains(ImportantChars.SINGLE_MARK_CHAR) && !contains(ImportantChars.QUOTE_MARK_CHAR)
     }
 }
